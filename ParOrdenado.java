7c4a6098092a2cae647049e62000506c6b56e08a
ElectronicaDC <electronica.titan@gmail.com>
2025-06-20 19:18:27 -0300

    segunda prueba en rama1

--- ParOrdenado.java
+++ ParOrdenado.java
@@ -1,8 +1,8 @@
 
 /**
 <<<<<<< HEAD
- *Tarea 10 del practico N1 PROBANDO QUE (rama1) FUNCIONE.
-=======
+ *Tarea 10 del practico N1 (rama1) 
+======= // prueba dos de parOrdenado
  *Tarea 10 del practico N1 (rama master)
 >>>>>>> 847563f65f6bf37e59e4ee1aeab271bec556823b
  */
